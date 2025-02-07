79bb4f91665fb7b2bdc6ea6acc49a0801a8f12bb
indigoBill <117851497+indigoBill@users.noreply.github.com>
2025-02-07 05:26:21 -0500

    Import ResumeEditor component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import ResumeEditor from './ResumeEditor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {
+      //<App />
+    }
+    <ResumeEditor />
   </StrictMode>,
 )
