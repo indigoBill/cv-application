fdf12a303de9c6a6db837d3cedf27bddc2ac4dae
indigoBill <117851497+indigoBill@users.noreply.github.com>
2025-02-21 06:47:00 -0500

    Update jsx components that will be rendered

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-//import App from './App.jsx'
-import ResumeEditor from './ResumeEditor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {
-      //<App />
-    }
-    <ResumeEditor />
+    <App />
   </StrictMode>,
 )
