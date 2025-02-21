748d0596088a9270c9803c72136bde2f6ad9806f
indigoBill <117851497+indigoBill@users.noreply.github.com>
2025-02-21 06:39:19 -0500

    Add general information input fields

--- src/ResumeEditor.jsx
+++ src/ResumeEditor.jsx
@@ -8,20 +8,41 @@ function ResumeEditorSwitch() {
                 <input type="checkbox" />
                 <span className="slider"></span>
             </label>
-            <p>COLOR</p>
+            <p>CUSTOMIZE</p>
         </div>
     )
 }
 
 function GeneralInformation() {
     return (
-        <div className='general-style general-info'>GENERAL INFO</div>
+        <div className='general-style general-info'>
+            <h3>GENERAL INFO</h3>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" />
+            </div>
+            <div>
+                <label htmlFor="jobTitle">Job Title:</label>
+                <input type="text" id="jobTitle" name="jobTitle" />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input type="text" id="email" name="email" />
+            </div>
+            <div>
+                <label htmlFor="phoneNumber">Phone:</label>
+                <input type="text" id="phoneNumber" name="phoneNumber" />
+            </div>
+        </div>
     )
 }
 
 function EducationalExperience() {
     return (
-        <div className='general-style educational-info'>EDUCATIONAL INFO</div>
+        <div className='general-style educational-info'>
+            <h3>EDUCATIONAL INFO</h3>
+        
+        </div>
     )
 }
 
@@ -34,7 +55,7 @@ function WorkExperience() {
 function ResumeEditor() {
     return (
         <div className='resume-editor-container'>
-            <ResumeEditorSwitch />
+            {/* {<ResumeEditorSwitch />} */}
             <GeneralInformation />
             <EducationalExperience />
             <WorkExperience />
