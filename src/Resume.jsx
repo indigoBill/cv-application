57fc6c44dbd9fd8a8c22369f325d2f7a42c68ae0
indigoBill <117851497+indigoBill@users.noreply.github.com>
2025-02-23 17:41:48 -0500

    Break Resume component into smaller components

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,8 +1,22 @@
 import './Resume.css';
 
+function ResumeGeneralInformation() {
+    return (
+        <>
+            <h1 className='resume-name'>NAME</h1>
+            <h2 className='resume-job-title'>JOB TITLE</h2>
+            <h3 className='resume-email'>EMAIL</h3>
+            <h3 className='resume-phone'>PHONE</h3>
+        </>
+    )
+}
+
 function Resume() {
     return (
-        <div className='resume'>RESUME</div>
+        <div className='resume'>
+            <ResumeGeneralInformation />
+        </div>
+       
     )
 }
 
