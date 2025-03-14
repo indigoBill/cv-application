7706de26b1900626d47ddd1690371dfb704b30b3
indigoBill <117851497+indigoBill@users.noreply.github.com>
2025-03-14 05:42:11 -0400

    Remove educational experience component

--- src/ResumeEditor.jsx
+++ src/ResumeEditor.jsx
@@ -15,38 +15,6 @@ function ResumeEditorSwitch() {
 }
 */
 
-function EducationalExperience() {
-    return (
-        <div className='general-style'>
-            <div className='educational-info'>
-                <h3>EDUCATIONAL INFO</h3>
-                <div>
-                    <label htmlFor="school-name">School Name:</label>
-                    <input type="text" id="school-name" name="school-name" />
-                </div>
-                <div>
-                    <label htmlFor="title-of-study">Title of Study:</label>
-                    <input type="text" id="title-of-study" name="title-of-study" />
-                </div>
-                <div className='educational-date-range'>
-                    <div>
-                        <label htmlFor="school-start-date">Start Date:</label>
-                        <input type="date" id="school-start-date" name="school-start-date" />
-                    </div>
-                    <div>
-                        <label htmlFor="school-end-date">End Date:</label>
-                        <input type="date" id="school-end-date" name="school-end-date" />
-                    </div>
-                </div>
-            </div>
-            <div className='edit-btns-container'>
-                <button className='save-school-btn'>Save</button>
-                <button className='add-school-btn'>Add Education</button>
-            </div>
-        </div>
-    )
-}
-
 function WorkExperience() {
     return (
         <div className='general-style'>
