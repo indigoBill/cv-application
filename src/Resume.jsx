c78723a89b6dff7609a44c8fa7d0f323a9dec39a
indigoBill <117851497+indigoBill@users.noreply.github.com>
2025-03-25 20:24:06 -0400

    Refactor ResumeEducationalInformation component

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -12,20 +12,30 @@ function ResumeGeneralInformation({ nameValue, jobTitleValue, emailValue, phoneV
 }
 
 function ResumeEducationalInformation({educationalValues}) {
-    const schoolNameValue = educationalValues[0];
-    const titleOfStudyValue = educationalValues[1];
-    const startDateValue = educationalValues[2];
-    const endDateValue = educationalValues[3];
-    let dash = '';
-
-    if(startDateValue && endDateValue != null) dash = '-';
-
     return (
         <>
-            <h1>{schoolNameValue}</h1>
-            <h2>{titleOfStudyValue}</h2>
-            <h2>{startDateValue}{dash}{endDateValue}</h2>
+            {
+                educationalValues.map((information) => {
+                    const schoolNameValue = information.school;
+                    const titleOfStudyValue = information.major;
+                    const startDateValue = information.start;
+                    const endDateValue = information.end;
+                    let dash = '';
+        
+                    if(startDateValue && endDateValue != null) dash = '-';
+                    
+                    return (
+                        <div key={information.id}>
+                            <h1>{schoolNameValue}</h1>
+                            <h2>{titleOfStudyValue}</h2>
+                            <h2>{startDateValue}{dash}{endDateValue}</h2>
+                        </div>
+                    )
+                })
+            }
+       
         </>
+        
     )
 }
 
