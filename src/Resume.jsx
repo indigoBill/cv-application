13d152b0f86b5217b73864a73821bf189e19b584
indigoBill <117851497+indigoBill@users.noreply.github.com>
2025-05-30 06:08:58 -0400

    Hide resume elements on page load

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,11 +1,19 @@
 import './Resume.css';
 
 function ResumeGeneralInformation({ nameValue, jobTitleValue, emailValue, phoneValue }) {
+    let displaySetting;
+
+    if(nameValue == ''){
+        displaySetting = 'hide';
+    }else{
+        displaySetting = 'general-design';
+    }
+
     return (
         <div className='resume-general-information'>
             <div>
                 <h1 className='resume-name'>{nameValue}</h1>
-                <div className='general-design'></div>
+                <div className={displaySetting}></div>
             </div>
             <div className='resume-information'>
                 <p className='resume-job-title'>{jobTitleValue}</p>
@@ -30,65 +38,68 @@ function ResumeObjectiveInformation({objective}) {
 }
 
 function ResumeEducationalInformation({educationalValues}) {
-    return (
-        <>
-            <h2 className='section-header'>EDUCATION:</h2>
-            {
-                educationalValues.map((information) => {
-                    const schoolNameValue = information.school;
-                    const titleOfStudyValue = information.major;
-                    const startDateValue = information.start;
-                    const endDateValue = information.end;
-                    let dash = '';
-        
-                    if(startDateValue && endDateValue != '') dash = '-';
-                    
-                    return (
-                        <div key={information.id} className='education-section'>
-                            <div className='school-date'>
-                                <p>{schoolNameValue}</p>
-                                <p>{startDateValue}{dash}{endDateValue}</p>
+    if(educationalValues.length != 0){
+        return (
+            <>
+                <h2 className='section-header'>EDUCATION:</h2>
+                {
+                    educationalValues.map((information) => {
+                        const schoolNameValue = information.school;
+                        const titleOfStudyValue = information.major;
+                        const startDateValue = information.start;
+                        const endDateValue = information.end;
+                        let dash = '';
+            
+                        if(startDateValue && endDateValue != '') dash = '-';
+                        
+                        return (
+                            <div key={information.id} className='education-section'>
+                                <div className='school-date'>
+                                    <p>{schoolNameValue}</p>
+                                    <p>{startDateValue}{dash}{endDateValue}</p>
+                                </div>
+                                <p>{titleOfStudyValue}</p>
                             </div>
-                            <p>{titleOfStudyValue}</p>
-                        </div>
-                    )
-                })
-            }
-       
-        </>
-        
-    )
+                        )
+                    })
+                }
+            </>
+        )
+    }
+    
 }
 
 function ResumeWorkInformation({workValues}) {
-    return (
-        <>
-            <h2 className='section-header'>WORK EXPERIENCE:</h2>
-            {
-                workValues.map((information) => {
-                    const companyNameValue = information.company;
-                    const positionValue = information.jobTitle;
-                    const startDateValue = information.start;
-                    const endDateValue = information.end;
-                    const description = information.responsibilities;
-                    let dash = '';
-        
-                    if(startDateValue && endDateValue != '') dash = '-';
-                    
-                    return (
-                        <div key={information.id} className='work-section'>
-                            <div className='work-date'>
-                                <p>{companyNameValue}</p>
-                                <p>{startDateValue}{dash}{endDateValue}</p>
+    if(workValues.length != 0){
+        return (
+            <>
+                <h2 className='section-header'>WORK EXPERIENCE:</h2>
+                {
+                    workValues.map((information) => {
+                        const companyNameValue = information.company;
+                        const positionValue = information.jobTitle;
+                        const startDateValue = information.start;
+                        const endDateValue = information.end;
+                        const description = information.responsibilities;
+                        let dash = '';
+            
+                        if(startDateValue && endDateValue != '') dash = '-';
+                        
+                        return (
+                            <div key={information.id} className='work-section'>
+                                <div className='work-date'>
+                                    <p>{companyNameValue}</p>
+                                    <p>{startDateValue}{dash}{endDateValue}</p>
+                                </div>
+                                <p>{positionValue}</p>
+                                <p>{description}</p>
                             </div>
-                            <p>{positionValue}</p>
-                            <p>{description}</p>
-                        </div>
-                    )
-                })
-            }
-        </>
-    )
+                        )
+                    })
+                }
+            </>
+        )
+    }
 }
 
 function ResumeSkillsInformation({skillValues}) {
